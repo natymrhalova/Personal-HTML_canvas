766f06db727af019b118f01381bee6c9c3385d9f
natymrhalova <naty.mrhalova@gmail.com>
2021-08-06 19:56:32 +0200

    big enemy shrinks on the first hit an dissapears one second to make the game more interesting

--- Canvas-game/app.js
+++ Canvas-game/app.js
@@ -69,10 +69,8 @@ class Enemy {
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
-const player = new Player(x, y, 30, "blue")
+const player = new Player(x, y, 30, "white")
 player.draw()
-
-const projectile = new Projectile(x, y, 5, "red", { x: 1, y: 1 })
 
 const projectiles = [];
 const enemies = [];
@@ -82,7 +80,7 @@ let spawnEnemies = () => {
         const x = Math.random() * canvas.width
         const y = 100
         const radius = Math.random() * (45 - 10) + 10;
-        const color = "green"
+        const color = `hsl(${Math.random() * 360}, 50%, 50%)`;
 
         const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x)
 
@@ -99,7 +97,8 @@ let animationId
 
 let animate = () => {
     animationId = requestAnimationFrame(animate)
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = "rgba(0,0,0, 0.1)"
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
     player.draw()
     projectiles.forEach((projectile, index) => {
         projectile.update()
@@ -123,25 +122,35 @@ let animate = () => {
         projectiles.forEach((projectile, projectileIndex) => {
             const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
 
+            //When projectiles touches enemy
             if (dist - enemy.radius - projectile.radius < 1) {
-                setTimeout(() => {
-                    enemies.splice(index, 1)
-                    projectiles.splice(projectileIndex, 1)
-                }, 0)
+
+                if (enemy.radius - 20 > 10) {
+                    enemy.radius -= 10
+                    setTimeout(() => {
+                        projectiles.splice(projectileIndex, 1)
+                    }, 0)
+                } else {
+                    setTimeout(() => {
+                        enemies.splice(index, 1)
+                        projectiles.splice(projectileIndex, 1)
+                    }, 0)
+
+                }
+
             }
         })
     })
-
 }
 
 addEventListener("click", (e) => {
     const angle = Math.atan2(e.clientY - y, e.clientX - x)
     const velocity = {
-        x: Math.cos(angle),
-        y: Math.sin(angle)
+        x: Math.cos(angle) * 5,
+        y: Math.sin(angle) * 5
     }
     projectiles.push(
-        new Projectile(x, y, 5, "red", velocity)
+        new Projectile(x, y, 5, "white", velocity)
     )
 })
 
